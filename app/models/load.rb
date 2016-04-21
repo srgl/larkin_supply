ff43829a22686efebf272401088eadf2431eb1f9
Sergei Lubianov <srglbnv@gmail.com>
2016-04-21 20:40:18 +0400

    Add validations and order number method

--- app/models/load.rb
+++ app/models/load.rb
@@ -2,4 +2,10 @@ class Load < ActiveRecord::Base
 	has_many :orders
 
 	enum delivery_shift: [:morning, :noon, :evening]
+
+  validates :delivery_date, :delivery_shift, presence: true
+
+  def load_number
+    "L#{delivery_date.strftime('%Y%m%d')}#{delivery_shift[0].upcase}"
+  end
 end
