8fc9c240c95d88d341fcabd18435c8621f7f97dc
Sergei Lubianov <srglbnv@gmail.com>
2016-04-22 20:46:01 +0400

    Add load validations

--- app/models/load.rb
+++ app/models/load.rb
@@ -4,8 +4,23 @@ class Load < ActiveRecord::Base
 	enum delivery_shift: [:morning, :noon, :evening]
 
   validates :delivery_date, :delivery_shift, presence: true
+  validate :delivery_shift_occupation, :volume_excess
+
+  AVAILABLE_VOLUME = 1400
 
   def load_number
     "L#{delivery_date.strftime('%Y%m%d')}#{delivery_shift[0].upcase}"
+  end
+
+  def delivery_shift_occupation
+    loads = Load.where(delivery_shift: delivery_shift, delivery_date: delivery_date)
+    loads = loads.where.not(id: id) if id
+    errors.add(:base, "Delivery shift is already occupied") if loads.count > 0
+  end
+
+  def volume_excess
+    if orders.where(load_id: id).sum(:volume) > AVAILABLE_VOLUME
+      errors.add(:base, "Available volume exceeded")
+    end
   end
 end
