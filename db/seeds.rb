18961d73a4c85772708edffc0505e0df506d4b29
Sergei Lubianov <srglbnv@gmail.com>
2016-04-25 18:10:49 +0400

    Fix users seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+if User.where(email: 'dispatcher@larkin.com').count == 0
+  dispatcher = User.new
+  dispatcher.email = 'dispatcher@larkin.com'
+  dispatcher.password = 'larkin'
+  dispatcher.password_confirmation = 'larkin'
+  dispatcher.save!
+end
 
-dispatcher = User.new
-dispatcher.email = 'dispatcher@larkin.com'
-dispatcher.password = 'larkin'
-dispatcher.password_confirmation = 'larkin'
-dispatcher.save!
-
-driver = User.new
-driver.email = 'driver@larkin.com'
-driver.password = 'larkin'
-driver.password_confirmation = 'larkin'
-driver.save!
+if User.where(email: 'driver@larkin.com').count == 0
+  driver = User.new
+  driver.email = 'driver@larkin.com'
+  driver.password = 'larkin'
+  driver.password_confirmation = 'larkin'
+  driver.save!
+end
