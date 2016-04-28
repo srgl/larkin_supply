61774a0d691b48d7b5e76ba39b964955ef380514
Sergei Lubianov <srglbnv@gmail.com>
2016-04-29 01:16:25 +0400

    Add constant storing company name

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module LarkinSupply
+  COMPANY_NAME = "Larkin LLC"
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
